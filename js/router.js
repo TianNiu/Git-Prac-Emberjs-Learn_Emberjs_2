d19b996352c019b8899ccf91a43a64208df17acf
SimBoo <thisiswoa@163.com>
2014-11-05 13:49:14 +0800

    getting started步骤--Adding Child Routes
    views下的edit_todo_view文件

--- js/router.js
+++ js/router.js
@@ -1,10 +1,20 @@
 //
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+    // additional child routes will go here later
+  });
+
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+  	/* 调用todos路由的model*/
+    return this.modelFor('todos');
   }
 });
