05b663eb83e4c1d943ade0740b0d0a55d076de9f
SimBoo <thisiswoa@163.com>
2014-11-05 14:12:52 +0800

    getting started步骤:Transitioning to Show Only Incomplete Todos

--- js/router.js
+++ js/router.js
@@ -1,20 +1,33 @@
-//
+/* map*/
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes will go here later
+    this.route('active');
   });
 
 });
-
+/* TodosRoute*/
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
   }
 });
-
+/* TodosIndexRoute*/
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
   	/* 调用todos路由的model*/
     return this.modelFor('todos');
   }
 });
+/* TodosActiveRoute*/
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
