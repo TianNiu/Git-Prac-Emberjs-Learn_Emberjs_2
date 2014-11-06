df1c22c696914816ac9b2564b908e6939cffade1
SimBoo <thisiswoa@163.com>
2014-11-06 17:25:39 +0800

    getting started步骤:Transitioning back to Show All Todos

--- js/router.js
+++ js/router.js
@@ -3,6 +3,7 @@ Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes will go here later
     this.route('active');
+    this.route('completed');
   });
 
 });
@@ -19,7 +20,10 @@ Todos.TodosIndexRoute = Ember.Route.extend({
     return this.modelFor('todos');
   }
 });
-/* TodosActiveRoute*/
+/** 
+ * TodosActiveRoute
+ * 未完成项路由
+ */
 Todos.TodosActiveRoute = Ember.Route.extend({
   model: function(){
     return this.store.filter('todo', function(todo) {
@@ -30,4 +34,19 @@ Todos.TodosActiveRoute = Ember.Route.extend({
     this.render('todos/index', {controller: controller});
   }
 });
+/** 
+ * TodosCompletedRoute
+ * 已完成项路由
+ */
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
 
